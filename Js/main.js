c8659ff6948ff3f7f08618b002d7ce66e3c26713
Bishal221 <kayalbishal221@gmail.com>
2023-06-26 21:23:05 +0530

    travel

---
+++ Js/main.js
@@ -0,0 +1,6 @@
+let language = document.querySelector(".language");
+let Click = document.getElementById("onclick");
+
+Click.onclick = function () {
+  language.classList.toggle("lan_display");
+};
